6bebc7786e3be1fd86c84a0f0d91128902268281
Anton Salianik <an2onsol@gmail.com>
2022-09-08 01:57:23 +0300

    feat: add solution to the fifth task

---
+++ fifth_problem.rb
@@ -0,0 +1,17 @@
+def smallest_multiple(first_num, second_num)
+  deviders = Array.new
+
+  for num in (first_num..second_num)
+    i = 1
+    deviders.each { |d| num /= d if num % d == 0}
+    while num != 1
+      if num % i == 0
+        num /= i
+        deviders << i
+      end
+      i += 1
+    end
+  end
+  deviders.inject(1){ |result, n| result * n }
+end
+
