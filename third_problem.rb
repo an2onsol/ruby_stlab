035003c9b25fe81699b7463828f1d9dd1ac672b5
Anton Salianik <an2onsol@gmail.com>
2022-09-08 10:55:41 +0300

    feat: add solution to the third task

---
+++ third_problem.rb
@@ -0,0 +1,14 @@
+def prime_factor(num)
+  if num < 2
+    raise ArgumentError.new "Input value lower than 2"
+  end
+  factor = 2
+  while num != 1
+    if num % factor == 0
+      num /= factor
+    else
+      factor += 1
+    end
+  end
+  return factor
+end
