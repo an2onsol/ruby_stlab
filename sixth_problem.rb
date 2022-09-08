1c8621f978e240d27b470438d420d35281ccf207
Anton Salianik <74258164+an2onsol@users.noreply.github.com>
2022-09-08 13:45:42 +0300

    fix: fix code style

--- sixth_problem.rb
+++ sixth_problem.rb
@@ -1,5 +1,5 @@
 def sum_square_diff(num)
   square_of_sum = ((1 + num) * num / 2)**2
-  sum_of_squares = (1..num).inject { |sum, n| sum + n**2}
+  sum_of_squares = (1..num).inject { |sum, n| sum + n**2 }
   square_of_sum - sum_of_squares
 end
